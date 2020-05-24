6c354679d9cb79bfaa38dcfff7e4e5137deaa2af
kwamkert <tomriddle477@gmail.com>
2020-05-24 22:54:39 +0000

    task module exported

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -25,7 +25,7 @@ const saveTask = async (req, res)  => {
 const findTask = async (req, res ) =>{
     let _id = req.params.id;
     try{
-        let task = await  Task.findOne({_id})
+        let task = await  Task.findOne({_id, owner: req.user._id})
         responseApi(res, 200, task, "Task found" )
     }catch(e){
         responseApi(res, 500, null, e.message)
@@ -37,7 +37,7 @@ const findTask = async (req, res ) =>{
 const updateTask = async (req, res) => {
     let _id = req.params.id
     try{
-        let task = await Task.findOneAndUpdate({_id}, req.body)
+        let task = await Task.findOneAndUpdate({_id, owner: req.user._id}, req.body)
         responseApi(res, 200, task, "Task updated")
     }catch(e){
         responseApi(res, 500, null, e.message)
@@ -56,4 +56,28 @@ const deleteTask = async (req, res) => {
         responseApi(res, 500, null, e.message)
     }
 
+}
+
+
+const userTasks = async (req, res) => {
+
+    try{
+        let tasks = await Task.find({owner: req.user._id});
+        tasks.length > 1 ? responseApi(res, 200, tasks, "Tasks found"):
+                           responseApi(res, 204, null, "No task found ")
+
+    }catch(e){
+        responseApi(res, 500, null, e.message)
+    }
+
+}
+
+
+
+module.exports = {
+    saveTask, 
+    getTask,
+    userTasks,
+    deleteTask,
+    updateTask
 }
