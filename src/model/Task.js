784349f13800867a2a33dad6d3ebe199d04ed3bb
kwamkert <tomriddle477@gmail.com>
2020-05-25 09:36:11 +0000

    due date added

--- src/model/Task.js
+++ src/model/Task.js
@@ -14,6 +14,10 @@ const taskSchema = new mongoose.Schema({
         enum: ['pending', 'ongoing', 'completed'],
         default: 'pending'
     },
+    dueDate: {
+        type: Date,
+        required: true
+    },
     category: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
